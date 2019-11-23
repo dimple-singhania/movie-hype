f2a808af2eed198cb00f797cd945a07c4107c0bc
Dimple Singhania <singhaniadimple5@gmail.com>
2019-11-22 16:31:41 -0800

    updates to get full text

--- Final_project.py
+++ Final_project.py
@@ -1,40 +1,32 @@
-
-#pip any library you need if you don't have it on your pc.
-import tweepy
-import twitter
-import json
 import tweepy
 import csv
-import pandas as pd
-consumer_key = 'DiIsBWy1EsatoOWKpXnM5pOx8' # your key and secret here
-consumer_secret = 'mc0qsu9pVFcHaOGDBGZBDTB80LhOBDfFSbMdEspDWIVgLLTtZg'
-access_token = '920748428807716864-1HJE6frWop2LPemyj8W3LMD57uWSEJn'
-access_token_secret = 'WvbHc3sIugEqDSpxT2YbnAdtdM7Jq7W0M986C9Bst5P7l'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+api_key = 'OUSwoGVeEJ6uQR102LedFpR2m'  # your key and secret here
+api_secret_key = 'Hcm7e0cnhwL4jCzOAHVyF4yelFK1gLXM07Vm2sEHXTzxOm2vj3'
+access_token = '202517150-87R2gouYsiYoQVSPVqWglNnU6GGEVism77AQJx5F'
+access_token_secret = 'idDj2JFm7bIfup5eL1QKKnNiWdHZKkNqDC8CEEPQa2Rhn'
+
+auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth,wait_on_rate_limit=True)
-"""
-for i in api.user_timeline(screen_name = 'realDonaldTrump',since = '2019-10-25',count = 10):
-    print(i.text)
-"""
-rows = ['date','content']
-with open('#MidwayMovie.csv','a') as f: #change the file name as movies name
-    ### I made a mistake here for some moive names, some whith a extra space at the begining, some not.###
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+rows = ['date', 'content']
+with open('Irishman_TEST.csv', 'a') as f:  # change the file name as movies name
 
     c = csv.writer(f, delimiter=',', lineterminator='\n')
-    c.writerow(rows)                        ##### pay attern about the space before #, I think I made mistake on this.
-    for tweet in tweepy.Cursor(api.search,q="#MidwayMovie", lang="en",#q is hashtag, search youself on twitter that
-                               # a movie's hashtag is
-                               # then change q as that hashtag
-                               since="2019-11-11",until = '2019-11-12').items():
-        "next data for DS is 117-118"
-        "next data for ternimate is 116-117"
-        "midway enxt is 12-13"
-        #change the since and unitl accordingly but day by day,
-        #otherwise the request will stop.
-        """
-        after every run, have a rest for about 10-15mins, then change the date and run again."""
-        print(tweet.created_at, tweet.text)
-        c.writerow ([tweet.created_at, tweet.text])
+    c.writerow(rows)
+    for tweet in tweepy.Cursor(api.search, q="#TheIrishman", lang="en", # CHANGE THE HASTAG FOR MOVIES HERE
+                               since="2019-11-20", until='2019-11-21',  # CHANGE THE DATES HERE
+                               tweet_mode='extended').items():
+        # change the since and unitl accordingly but day by day,
+        # otherwise the request will stop.
+
+        # print(tweet.created_at, tweet.text)
+        try:
+            content = tweet.full_text
+        except AttributeError:
+            content = tweet.text
+        c.writerow([tweet.created_at, content])
 f.close()
+
+# Need to remove duplicate rows
